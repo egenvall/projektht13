b06dd3d0cb46c259f5866da09c37a51a006dd520
egenvall <kim.egenvall@hotmail.com>
2013-09-19 11:01:49 +0200

    Massa onödigt med, men fungerande version

--- Projektht13/src/com/example/projektht13/MainActivity.java
+++ Projektht13/src/com/example/projektht13/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.projektht13;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/*Test*/
 	}
 
 	@Override
