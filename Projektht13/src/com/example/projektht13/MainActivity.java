54ea9a4433f565e4902c2a79df518b758db2e681
egenvall <kim.egenvall@hotmail.com>
2013-09-19 14:41:42 +0200

    Updated gitignore, added menu items

--- Projektht13/src/com/example/projektht13/MainActivity.java
+++ Projektht13/src/com/example/projektht13/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.projektht13;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
-
+	GoogleMap map;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		   map = ((SupportMapFragment) getSupportFragmentManager()
+	                .findFragmentById(R.id.map)).getMap();
+	        if (map == null) {
+	            Toast.makeText(this, "Google Maps not available", 
+	                Toast.LENGTH_LONG).show();
+	        }
 		
 		/*Test*/
 	}
