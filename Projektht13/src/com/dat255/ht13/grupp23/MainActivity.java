7b58675bc0fddc5ef20e9883c4aba5ea3b297831
timocin <timocin.zaynal@me.com>
2013-09-24 13:47:50 +0200

    Fixat sa att maps hittar sin egen position som default(centrerad pasig sjalv)

--- Projektht13/src/com/dat255/ht13/grupp23/MainActivity.java
+++ Projektht13/src/com/dat255/ht13/grupp23/MainActivity.java
@@ -1,36 +1,114 @@
 package com.dat255.ht13.grupp23;
 
 import com.dat255.ht13.grupp23.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
-	GoogleMap map;
+public class MainActivity extends FragmentActivity implements LocationListener {
+	GoogleMap googleMap;
 	Controller controller;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// Getting Google Play availability status
+		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
-		map = ((SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map)).getMap();
-		if (map == null) {
-			Toast.makeText(this, "Google Maps not available", Toast.LENGTH_LONG)
-					.show();
+		// Showing status
+		if(status!=ConnectionResult.SUCCESS){ // Google Play Services are not available
+
+			int requestCode = 10;
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+			dialog.show();
+
+		}else { // Google Play Services are available
+
+			// Getting reference to the SupportMapFragment of activity_main.xml
+			SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+			// Getting GoogleMap object from the fragment
+			googleMap = fm.getMap();
+
+			// Enabling MyLocation Layer of Google Map
+			googleMap.setMyLocationEnabled(true);
+
+			// Getting LocationManager object from System Service LOCATION_SERVICE
+			LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+			// Creating a criteria object to retrieve provider
+			Criteria criteria = new Criteria();
+
+			// Getting the name of the best provider
+			String provider = locationManager.getBestProvider(criteria, true);
+
+			// Getting Current Location
+			Location location = locationManager.getLastKnownLocation(provider);
+
+			if(location!=null){
+				onLocationChanged(location);
+			}
+			locationManager.requestLocationUpdates(provider, 20000, 0, this);
 		}
+
 
 		/* Test */
 
-		controller = new Controller();
-		controller.initiate();
+		//controller = new Controller();
+		//controller.initiate();
 	}
+	
+	@Override
+    public void onLocationChanged(Location location) {
+ 
+        // Getting latitude of the current location
+        double latitude = location.getLatitude();
+ 
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+ 
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+ 
+        // Showing the current location in Google Map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+ 
+        // Zoom in the Google Map
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+ 
+    }
+ 
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+ 
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+ 
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+ 
+   
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
