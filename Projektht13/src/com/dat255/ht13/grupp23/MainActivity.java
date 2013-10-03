236e647bc235b8ba1354284dc5bf8474f79897a2
Carl Fredriksson <c@msp.se>
2013-10-03 16:54:34 +0200

    Made the controller the activity instead of the view, according to how most people go about android and mvc

--- Projektht13/src/com/dat255/ht13/grupp23/MainActivity.java
+++ Projektht13/src/com/dat255/ht13/grupp23/MainActivity.java
@@ -1,17 +1,17 @@
 package com.dat255.ht13.grupp23;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends FragmentActivity{
-	Controller controller;
-	
+public class MainActivity extends FragmentActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		finish();
-		controller = Controller.getInstance();
-		controller.initiate(this);
-		
+		Intent mapController = new Intent(this, MapController.class);
+		startActivity(mapController);
 	}
 
 }
