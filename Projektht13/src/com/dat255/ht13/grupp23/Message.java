28e5ba10f9be60a8de37db5a733b68066d29144a
Carl Fredriksson <c@msp.se>
2013-10-11 17:28:29 +0200

    Added name to Message

--- Projektht13/src/com/dat255/ht13/grupp23/Message.java
+++ Projektht13/src/com/dat255/ht13/grupp23/Message.java
@@ -2,13 +2,18 @@ package com.dat255.ht13.grupp23;
 
 import java.util.Date;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 public class Message {
 
+	private final String defaultName = "Anonymous";
+	private final String defaultText = "";
+
+	private String name;
 	private String text;
 	private Date date;
+
+	public String getName() {
+		return name;
+	}
 
 	public String getText() {
 		return text;
@@ -27,7 +32,8 @@ public class Message {
 	 * Default constructor.
 	 */
 	public Message() {
-		text = "";
+		name = defaultName;
+		text = defaultText;
 		date = new Date();
 	}
 
@@ -38,6 +44,7 @@ public class Message {
 	 *            the text of the Message.
 	 */
 	public Message(String text) {
+		name = defaultName;
 		this.text = text;
 		date = new Date();
 	}
@@ -52,6 +59,38 @@ public class Message {
 	 *            the date of the Message.
 	 */
 	public Message(String text, Date date) {
+		name = defaultName;
+		this.text = text;
+		this.date = date;
+	}
+
+	/**
+	 * Constructor with specified message name and text.
+	 * 
+	 * @param name
+	 *            the name of the author
+	 * @param text
+	 *            the text of the Message.
+	 */
+	public Message(String name, String text) {
+		this.name = name;
+		this.text = text;
+		date = new Date();
+	}
+
+	/**
+	 * Constructor with specified message name, text and date. Is used when data
+	 * from the database needs to be recreated as a Message object.
+	 * 
+	 * @param name
+	 *            the name of the author
+	 * @param text
+	 *            the text of the Message.
+	 * @param date
+	 *            the date of the Message.
+	 */
+	public Message(String name, String text, Date date) {
+		this.name = name;
 		this.text = text;
 		this.date = date;
 	}
@@ -63,6 +102,7 @@ public class Message {
 	 *            the Message object to be copied.
 	 */
 	public Message(Message message) {
+		this.name = message.getName();
 		this.text = message.getText();
 		this.date = message.getDate();
 	}
@@ -77,7 +117,8 @@ public class Message {
 		}
 		if (object instanceof Message) {
 			Message other = (Message) object;
-			if (this.text.equals(other.getText())
+			if (this.name.equals(other.getName())
+					&& this.text.equals(other.getText())
 					&& this.date.equals(other.getDate())) {
 				return true;
 			}
@@ -89,9 +130,11 @@ public class Message {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((date == null) ? 0 : date.hashCode());
 		result = prime * result + ((text == null) ? 0 : text.hashCode());
 		return result;
 	}
 
 }
+
