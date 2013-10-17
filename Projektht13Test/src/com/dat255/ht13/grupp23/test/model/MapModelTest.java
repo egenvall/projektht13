3b82fe670135d0441f99025cc9c9f2db83413c0e
Johan Vallander <johan.vallander@gmail.com>
2013-10-17 14:51:48 +0200

    updated MapModelTest, included test for db, it is an untested test

--- Projektht13Test/src/com/dat255/ht13/grupp23/test/model/MapModelTest.java
+++ Projektht13Test/src/com/dat255/ht13/grupp23/test/model/MapModelTest.java
@@ -18,7 +18,7 @@ public class MapModelTest extends TestCase{
 	
 	Point point1;
 	Point point2;
-	
+	Point spacePoint;
 	Message msg1;
 	Message msg2;
 	
@@ -32,32 +32,26 @@ public class MapModelTest extends TestCase{
 		model = new MapModel();
 		point1 = new Point(11.11,11.11);
 		point2 = new Point(22.22,22.22);
+	    spacePoint = new Point(151251,1251251);
+	    spaceMessage = new Message("I'm posting this from outher space"); //spacePoint and spaceMessage are intended to be pushed to the database without ever interfering with any position on earth (or in reality)
 		msg1 = new Message("Test1");
 		msg2 = new Message("Test2");
 	}
-
+	
+    @Test
+    public void testAddMessagePoint(){
+        model.AddMessagePoint(point1);
+        model.AddMessagePoint(point2);
+        assertEquals(2,model.getMessagePoints().size())
+    }
+    
+	
 	@Test
 	public void testGetMessagePoints() {
 		model.AddMessagePoint(point1);
-		assertEquals(0, model.getMessagePoints().size());
-	}
-
-	@Test
-	public void testAddMessagePoint_msg() {
-		model.AddMessagePoint(point1);
-		model.AddMessagePoint(point2);
-		assertTrue(model.getMessagePoints() instanceof ArrayList);
-		assertEquals(2, model.getMessagePoints().size());
-	}
-	@Test
-	public void testRemoveMessagePoint() {
-		model.AddMessagePoint(point1);
-		model.AddMessagePoint(point2);
-		assertEquals(2, model.getMessagePoints().size());
-		
-		model.RemoveMessagePointById(1);
 		assertEquals(1, model.getMessagePoints().size());
 	}
+	
 	@Test
 	public void testAddMessageToMessagePoint() {
 		model.AddMessagePoint(point1);
@@ -67,5 +61,40 @@ public class MapModelTest extends TestCase{
 		assertTrue(model.getMessagePoints() instanceof ArrayList);
 		assertEquals(2, model.getMessagePoints().size());
 		assertEquals(msg1, model.getMessagePointById(0).getMessages().get(0));
+	}
+	
+	@Test(timeout=15)
+	public void testDatabaseConnection(){ //test connections to the database
+	    int inititalMPSize = model.getMessagePoints().size();
+	    model.updateMP();
+        while(model.getMessagePoints().size() == initialMPSize); // loop until size changes or until timeout
+	    int initialSize = model.getMessagePoints().size();
+	    model.addMessagePoint(spacePoint);
+	    //find the id for that point;
+	    ArrayList<MessagePoint> messagePoints = model.getMessagePoints();
+	    MessagePoint messagePoint = null;
+	    for(MessagePoint mp : messagePoints){
+	        if(mp.getPosition.getX() == spacePoint.getX() && mp.getPosition.getY() == spacePoint.getY()){
+	            messagePoint = mp;
+	            break;
+	        }
+	    }
+	    int id = messagePoint.getId();
+	    model.addMessageToMessagePoint(id,spaceMessage);
+	    Thread.sleep(2000);
+        //Test if we can download the same data to a new model
+        MapModel newModel = new MapModel();
+        assertEquals(newModel.getMessagePoints.size(),0); //assert that it is empty
+        inititalMPSize = newModel.getMessagePoints().size();
+	    newModel.updateMP();
+        while(newModel.getMessagePoints().size() == initialMPSize); // loop until size changes or until timeout
+        MessagePoint spaceMessagePoint=newModel.getMessagePointById(id);
+        assertEquals( spaceMessagePoint.getPosition().getX(),spacePoint.getX());
+        assertEquals(spaceMessagePoint.getPosition().getY(),spacePoint.getY());
+        Message message = spaceMessagePoint.getMessages().get(0);
+        assertEquals(message.getText(),spaceMessage.getText());
+ 
+	    
+	
 	}
 }
